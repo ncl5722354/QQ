417efa328573e992045c5054807447cbd482df3f
ncl5722354 <897117296@qq.com>
2021-04-28 09:32:32 +0800

    remote

---
+++ QQback/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+using namespace std;
+
+string const url = "58.250.100.13:3306";
+string const database = "qqdatabase";
+string const username = "root";
+string const passwrod = "ncl5722354";
+
+int main()
+{
+	
+	
+
+	return 0;
+}
